2167c8c891efeb8677b16c64876eae173df5571f
canberkok <78238011+canberkok@users.noreply.github.com>
2021-02-14 02:42:41 +0300

    Create ekrana-rastgele-sayi.c

---
+++ ekrana-rastgele-sayi.c
@@ -0,0 +1,18 @@
+include<stdio.h>
+include<conio.h>
+include <stdlib.h>
+include <time.h>
+int main()
+{  int rastgele ;
+    int girilensayi ;
+   rastgele=srand(time(NULL) ;
+printf("Ekrana gelen değeri giriniz\n");
+printf("%d",rastgele) ;
+scanf( " %d " , &girilensayi );
+if ( girilensayi=rastgele)  
+ printf("DogruGiris") ;
+else 
+printf("HataliGiris");
+return 0 ;
+
+}
